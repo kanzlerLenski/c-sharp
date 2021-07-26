d629c4489258dc7fb8218f439fa29adb78f68d79
kanzlerlenski <44209042+kanzlerlenski@users.noreply.github.com>
2021-07-26 23:54:39 +0300

    Add files via upload

---
+++ Прочее/WindowsFormsApplication46/WindowsFormsApplication46/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication46
+{
+      struct Lex
+
+    {
+        public string word;
+        public string lex;
+        public string gr;
+        public string sem;
+
+
+    public Lex(string word, string lex, string gr, string sem)
+    {
+        this.word = word;
+        this.lex = lex;
+        this.gr = gr;
+        this.sem = sem;
+
+    }
+     }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader a = new StreamReader(@"E:\Программирование\RusCorpora.txt", Encoding.Default);
+
+    {
+        Lex m = new Lex("Маша", "Маша", "S sing Nom", "pers hum");
+        Lex k = new Lex("кошкой", "кошка", "S sing f Inst", "com anim");
+
+        List<Lex> le = new List<Lex> {m, k};
+        
+        foreach (Lex l in le)
+        {
+            richTextBox1.Text = ("word: " + l.word + ", lex: " + l.lex + ", gr: " + l.gr + ", sem: " + l.sem);
+
+    }
+
+        }
+    }
+}
+
+}
---
+++ Прочее/WindowsFormsApplication471/WindowsFormsApplication47/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication47
+{
+
+    public struct MyStruct
+    {
+        /*объявление структуры*/
+        public string lex;
+        public string sem;
+        public string gr;
+
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            /*экземпляр струтуры*/
+            MyStruct myStruct1 = new MyStruct();
+            myStruct1.lex = "Светлана";
+            myStruct1.sem = "";
+            myStruct1.gr = "";
+
+            MyStruct myStruct2 = new MyStruct();
+            myStruct2.lex = "Светлана";
+            myStruct2.sem = "";
+            myStruct2.gr = "";
+
+            List<MyStruct> myStructs = new List<MyStruct>();
+            myStructs.Add(myStruct1);
+            myStructs.Add(myStruct2);
+        }
+    }
+}
